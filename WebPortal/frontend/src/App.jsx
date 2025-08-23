b06ee7141f85dd49fd9604341bc8b93505519281
PriyankGupta22 <gsanu8092@gmail.com>
2025-08-23 16:26:39 +0530

    worked on the podium component

--- WebPortal/frontend/src/App.jsx
+++ WebPortal/frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
+import gold from './assets/gold.svg'
+import sil from './assets/silver.svg'
+import bron from './assets/bronze.svg'
+import lig from './assets/lig.svg'
 import NavBar from './components/NavBar/NavBar'
 import TitleScreen from './components/TitleScreen/TitleScreen'
 import About from "./components/About/About"
+import Bar from "./components/Bar/Bar.jsx"
 function App() {
 
   return (
    <div>
-    <NavBar/>
-    <TitleScreen/>
-    <About/>
+    <Bar pos={gold} name="user_name" user="@user_id" icon={lig} sco="12345" place="1" />
    </div>
   )
 }
