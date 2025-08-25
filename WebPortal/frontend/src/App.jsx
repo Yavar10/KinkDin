63ff1e89efe79b00ec3c1c7483a58823e64bb349
Yavar <zuri00014@gmail.com>
2025-08-26 00:32:00 +0530

    added theme change functionality

--- WebPortal/frontend/src/App.jsx
+++ WebPortal/frontend/src/App.jsx
@@ -7,16 +7,23 @@ import SignUpPage from './pages/Sign/SignUpPage.jsx'
 import SignInPage from './pages/Sign/SignInPage.jsx'
 
 import { ToastContainer } from 'react-toastify'
+import { useState } from 'react'
+import { useContext } from 'react'
+import { themeContext } from './components/Context/context.js'
 
 function App() {
 
+  const [colo,setColo]=useState("#0f051e")
+
   return (
-   <div>
+   <div style={{background:colo}}>
+    <themeContext.Provider value={{colo,setColo}}>
      <Routes>
         <Route path='/'element={<Home/>} />
         <Route path='/SignUp'element={<SignUpPage/>} />
         <Route path='/SignIn'element={<SignInPage/>} />
       </Routes>
+      </themeContext.Provider>
       <ToastContainer position="top-center" autoClose={3000} />
    </div>
   )
