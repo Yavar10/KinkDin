2d43ad12ca7d4693379e0daf62055c35f96048d7
Yavar <zuri00014@gmail.com>
2025-09-02 23:50:11 +0530

    added avatar inclusion

--- WebPortal/frontend/src/components/RankingBox/RankingBox.jsx
+++ WebPortal/frontend/src/components/RankingBox/RankingBox.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import "./RankingBox.css"
 
-import yavar from '../../assets/temp pfps/yavar.jpg'
+//import yavar from '../../assets/temp pfps/yavar.jpg'
 
 const RankingBox = ({value}) => {
-    const {rank,name,easy,medium,hard}=value;
+    const {rank,name,easy,medium,hard,img}=value;
     const locks=easy*50+ medium*100 + hard*200;
 
   return (
@@ -13,7 +13,7 @@ const RankingBox = ({value}) => {
       <div className='RankBox'>
         <div className='pd'>
         <p>{rank}</p>
-        <img className='img-rank' src={yavar} alt="" />
+        <img className='img-rank' src={img} alt="" />
         <p>{name}</p>
         </div>
         <div className="ques-data">
